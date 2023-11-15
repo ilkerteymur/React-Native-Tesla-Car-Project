eedacb23451d0593d1bba460f7a75d8653aaef61
ilkerteymur <ilkerteymuroglu@gmail.com>
2023-11-16 00:01:11 +0300

    car-component-done

--- App.tsx
+++ App.tsx
@@ -6,7 +6,12 @@ import CarItem from './components/CarItem';
 function App(): JSX.Element {
   return (
     <View style={styles.container}>
-      <CarItem />
+      <CarItem
+        name={'Model X'}
+        tagline={'Order Online For'}
+        tagLineCTA={'Touchless Delivery'}
+        image={require('./assets/images/Model3.jpeg')}
+      />
     </View>
   );
 }
--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -4,32 +4,34 @@ import styles from './styles';
 import StyledButton from '../StyledButton';
 
 const CarItem = props => {
+  const {name, tagline, tagLineCTA, image} = props;
   return (
     <View style={styles.carContainer}>
-      <ImageBackground
-        source={require('../../assets/images/ModelX.jpeg')}
-        style={styles.image}
-      />
+      <ImageBackground source={image} style={styles.image} />
       <View style={styles.titles}>
-        <Text style={styles.title}>Model S</Text>
-        <Text style={styles.subtitle}>Starting at $69,420</Text>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>
+          {tagline} <Text style={styles.subtitleCTA}>{tagLineCTA} </Text>
+        </Text>
       </View>
 
-      <StyledButton
-        type="primary"
-        content={'Custom Order'}
-        onPress={() => {
-          console.warn('pressed');
-        }}
-      />
+      <View style={styles.buttonsContainer}>
+        <StyledButton
+          type="primary"
+          content={'Custom Order'}
+          onPress={() => {
+            console.warn('pressed');
+          }}
+        />
 
-      <StyledButton
-        type="secondary"
-        content={'Existing Inventory'}
-        onPress={() => {
-          console.warn(' existing pressed');
-        }}
-      />
+        <StyledButton
+          type="secondary"
+          content={'Existing Inventory'}
+          onPress={() => {
+            console.warn(' existing pressed');
+          }}
+        />
+      </View>
     </View>
   );
 };
